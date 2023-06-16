c2141448bcf8536af906f55419d1ed36180294a6
AndresCooprofesores <andrescar18@gmail.com>
2023-06-16 16:44:02 -0500

    commit con .env configurado para la base de datos

--- src/config.js
+++ src/config.js
@@ -1,11 +1,18 @@
 import { config } from 'dotenv';
+require('dotenv').config();
 
 config();
 
-module.export = {
-  POSTGRES_URI: process.env.POSTGRES_URI
-  
-};
+export const conectionData2 = {
+  user: process.env.userC,
+  password: process.env.passwordC,
+  host: process.env.hostC,
+  port: process.env.portC,
+  database: process.env.databaseC
+}
+
+//module.export = conectionData2;
+  //POSTGRES_URI: process.env.POSTGRES_URI,
 
 export const PORT = process.env.PORT || 3000;
 
--- src/db.js
+++ src/db.js
@@ -1,14 +1,15 @@
 const { Pool } = require('pg');
-const config = require('./config');
+const config  = require('./config');
+const conectionData2 =require('./config');
 
-const pool = new Pool({
-  // coloque aca la variable a conectar
-  //connectionString: config.POSTGRES_URI 
-  user: 'root',
-  password: 'root',
-  host: '192.168.0.154',
-  port: 5430,
-  database: 'consecutivos'
+const conectionData = config.conectionData2;
+
+const pool = new Pool({// coloque aca la variable a conectar
+  user: conectionData.user,
+  password: conectionData.password,
+  host: conectionData.host,
+  port: conectionData.port,
+  database: conectionData.database
 });
 
 

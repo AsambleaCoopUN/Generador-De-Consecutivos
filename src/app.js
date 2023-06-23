231e1bf67261444860b3ccba41fc52f155eb5494
khronos1024 <jeanvesu@gmail.com>
2023-06-23 17:26:38 -0500

    cambio sustancial del front para mejor aspecto visual

--- src/app.js
+++ src/app.js
@@ -8,4 +8,15 @@ app.use(express.static(path.join(__dirname, '../node_modules/bootstrap/')));
 app.use(express.static(path.join(__dirname, '../node_modules/jquery/')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* indicación para la captura de datos con método post */
+const { json } = require("express");
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+/* EJS como motor de plantillas*/
+app.set('view engine','ejs');
+
+/* llamado del enrutador */
+app.use('/', require('./router'));
+
 export default app;

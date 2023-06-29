19ea40f1c8eba398f265156ad5b89be69e79ff25
khronos1024 <jeanvesu@gmail.com>
2023-06-29 10:07:34 -0500

    se agrega vista de login

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,11 @@
 const consecRouter = require('./consecRouter');
 const mainRouter = require('./mainRouter');
+const loginRouter = require('./loginRouter');
 
 function routerApp (app){
-  app.use('/list', consecRouter);
   app.use('/', mainRouter);
+  app.use('/login', loginRouter);
+  app.use('/list', consecRouter);
 }
 
 module.exports = routerApp;

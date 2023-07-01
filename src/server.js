b83dafc9c96ad52e9787566ce66339eeb170832e
Andres M. Cardenas <andrescar18@gmail.com>
2023-07-01 16:25:31 -0500

    se modifica conexion al LDAP

--- src/server.js
+++ src/server.js
@@ -1,24 +1,25 @@
 const ActiveDirectory = require('activedirectory');
-const config  = require('./config');
-const adConectConfig =require('./config');
+const config = require('./config');
 
-const adConect = config.adConectConfig;
+const adConectConfig = config.adConectConfig;
 
-// const adConect = {
-//   url: 'ldap://192.168.0.65',
-//   baseDN:'DC=cooprofesoresun,DC=coop',
-//   username: 'glpi',
-//   password: 'Cooprofesores2023$',
-// };
+const username = "nombreDeUsuario";
+const password = "contraseñaDelUsuario";
+
+const adConect = {
+  url: adConectConfig.user,
+  baseDN: adConectConfig.baseDN,
+  username: adConectConfig.username,
+  password: adConectConfig.password,
+};
 console.log(adConect);
 
 /* Verifica las credenciales del usuario en el Directorio Activo */
-const ad = new ActiveDirectory(adConfig);
+const ad = new ActiveDirectory(adConect);
 ad.authenticate(username, password, (error, auth) => {
   if (error) {
     console.log(error);
   }
-
   if (!auth) {
     console.log("usuario o contraseña invalidos");
   } else {

796abc78a435827322339c2d55449c5f2ec5ca36
khronos1024 <jeanvesu@gmail.com>
2023-07-02 12:24:52 -0500

    se agrega la lista de prefijos para posterior modificación

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -4,6 +4,7 @@ const pool = require('../src/db');
 
 let cedula;
 let idUser;
+let message;
 
 router.post('/', (req, res)=>{
   cedula = req.body.user;
@@ -19,31 +20,50 @@ router.post('/', (req, res)=>{
     console.log('Conexión exitosa a la base de datos')
   });
   
-  pool.query(user,(error, resultUser)=>{    
-    idUser = resultUser.rows[0].usuario_id;
+  try{
+    pool.query(user,(error1, resultUser)=>{
+      if(!error1){
+        idUser = resultUser.rows[0].usuario_id;  
+      }else{
+        console.log(error1);
+        message = 'Usuario no encontrado';
+        res.send(`<script>
+                  if(confirm('${message}')){
+                    window.location.href='/login';
+                  }else{
+                    window.location.href='/login';
+                  }
+                  </script>`);
+      };
+    });
 
-    const history = `select u.usuario_nombre, p.prefijo, hc.consecutivo, hc.descripcion, 
-    cast (hc.fecha_generacion as VARCHAR(16)) 
-    AS fecha
-    from consecutivo.usuario u,consecutivo.prefijo p,consecutivo.historia_consecutivo hc
-    where hc.prefijo_id = p.prefijo_id  and hc.usuario_id = u.usuario_id
-    and u.usuario_id = '${idUser}'
-    order by fecha_generacion desc`;
-
-    try {
-      pool.query(history,(error, result)=>{
-        try{
-          res.render('users', {result:result.rows});
-        }catch (error) {
-          console.log(error);
-          res.render('index');
-        }
-      });
-    } catch (error) {
-      console.log(error);
-      res.render('index');
-    };
-  });
+      const history = `select u.usuario_nombre, p.prefijo, hc.consecutivo, hc.descripcion, 
+      cast (hc.fecha_generacion as VARCHAR(16)) 
+      AS fecha
+      from consecutivo.usuario u,consecutivo.prefijo p,consecutivo.historia_consecutivo hc
+      where hc.prefijo_id = p.prefijo_id  and hc.usuario_id = u.usuario_id
+      and u.usuario_id = '${idUser}'
+      order by fecha_generacion desc`;
+      
+    pool.query(history,(error2, result)=>{
+      if(!error2){
+        res.render('users', {result:result.rows});
+      }else{
+        message = 'Usuario no encontrado';
+        console.log(error2);
+        res.send(`<script>
+                  if(confirm('${message}')){
+                    window.location.href='/login';
+                  }else{
+                    window.location.href='/login';
+                  }
+                  </script>`);
+      }
+    });
+  }catch{
+    console.log(error1,error2);
+    res.render('login');
+  }
 });
 
 module.exports = router;

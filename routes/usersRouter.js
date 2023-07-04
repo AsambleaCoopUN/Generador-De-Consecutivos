70a65703c4bd479e036b87a08d361b9534faec0a
khronos1024 <jeanvesu@gmail.com>
2023-07-04 16:30:38 -0500

    ajustes a la ruta y validación

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,6 +5,7 @@ const pool = require('../src/db');
 let cedula;
 let idUser;
 let message;
+let history;
 
 router.post('/', (req, res)=>{
   cedula = req.body.user;
@@ -20,37 +21,44 @@ router.post('/', (req, res)=>{
     console.log('Conexión exitosa a la base de datos')
   });
   
-  try{
-    pool.query(user,(error1, resultUser)=>{
-      if(!error1){
-        idUser = resultUser.rows[0].usuario_id;  
-      }else{
-        console.log(error1);
-        message = 'Usuario no encontrado';
-        res.send(`<script>
+  pool.query(user,(error1, resultUser)=>{   
+    if (!error1){
+      idUser = resultUser.rows[0].usuario_id;
+      history = `select u.usuario_nombre, p.prefijo, hc.consecutivo, hc.descripcion, hc.fecha_generacion 
+        from consecutivo.usuario u,consecutivo.prefijo p,consecutivo.historia_consecutivo hc
+        where hc.prefijo_id = p.prefijo_id  and hc.usuario_id = u.usuario_id
+        and u.usuario_id = '${idUser}'
+        order by fecha_generacion desc`;
+
+    }else{
+      console.log(error1);
+      message = 'Usuario no encontrado';
+      res.send(`<script>
+                if(confirm('${message}')){
+                    window.location.href='/login';
+                  }else{
+                    window.location.href='/login';
+                  }
+                  </script>`);
+    };
+    
+    pool.query(history,(error2, result)=>{
+      if (!error2){
+        if (result!=""){
+          res.render('users', {result:result.rows});
+        }else{
+          message = 'Datos no encontrado';
+          res.send(`<script>
                   if(confirm('${message}')){
                     window.location.href='/login';
                   }else{
                     window.location.href='/login';
                   }
                   </script>`);
-      };
-    });
-
-      const history = `select u.usuario_nombre, p.prefijo, hc.consecutivo, hc.descripcion, 
-      cast (hc.fecha_generacion as VARCHAR(16)) 
-      AS fecha
-      from consecutivo.usuario u,consecutivo.prefijo p,consecutivo.historia_consecutivo hc
-      where hc.prefijo_id = p.prefijo_id  and hc.usuario_id = u.usuario_id
-      and u.usuario_id = '${idUser}'
-      order by fecha_generacion desc`;
-      
-    pool.query(history,(error2, result)=>{
-      if(!error2){
-        res.render('users', {result:result.rows});
-      }else{
-        message = 'Usuario no encontrado';
+        }
+      }else {
         console.log(error2);
+        message = 'Datos no encontrado';
         res.send(`<script>
                   if(confirm('${message}')){
                     window.location.href='/login';
@@ -59,11 +67,8 @@ router.post('/', (req, res)=>{
                   }
                   </script>`);
       }
-    });
-  }catch{
-    console.log(error1,error2);
-    res.render('login');
-  }
+    });  
+  });
 });
 
 module.exports = router;

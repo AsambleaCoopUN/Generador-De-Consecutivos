8ff69d622600e18dccb98cf1c1e835fc1a352033
khronos1024 <jeanvesu@gmail.com>
2023-07-04 16:51:29 -0500

    ajuste a la cunsulta para el renderizado del usuario

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -10,9 +10,9 @@ let history;
 router.post('/', (req, res)=>{
   cedula = req.body.user;
 
-  const user = `select u.usuario_id
-    from consecutivo.usuario u
-    where u.usuario_identificacion  = '${cedula}'`;
+  const user = `select u.usuario_nombre, u.usuario_id 
+  from consecutivo.usuario u
+  where u.usuario_identificacion  = '${cedula}'`;
 
   pool.connect((err, client, release) => {
     if (err) {
@@ -20,15 +20,17 @@ router.post('/', (req, res)=>{
     }
     console.log('Conexión exitosa a la base de datos')
   });
-  
+
   pool.query(user,(error1, resultUser)=>{   
     if (!error1){
       idUser = resultUser.rows[0].usuario_id;
-      history = `select u.usuario_nombre, p.prefijo, hc.consecutivo, hc.descripcion, hc.fecha_generacion 
-        from consecutivo.usuario u,consecutivo.prefijo p,consecutivo.historia_consecutivo hc
-        where hc.prefijo_id = p.prefijo_id  and hc.usuario_id = u.usuario_id
-        and u.usuario_id = '${idUser}'
-        order by fecha_generacion desc`;
+      history = `select u.usuario_nombre, p.prefijo, hc.consecutivo, hc.descripcion, 
+      cast (hc.fecha_generacion as VARCHAR(16))
+      as fecha 
+      from consecutivo.usuario u,consecutivo.prefijo p,consecutivo.historia_consecutivo hc
+      where hc.prefijo_id = p.prefijo_id  and hc.usuario_id = u.usuario_id
+      and u.usuario_id = '${idUser}'
+      order by fecha_generacion desc`;
 
     }else{
       console.log(error1);
@@ -45,7 +47,7 @@ router.post('/', (req, res)=>{
     pool.query(history,(error2, result)=>{
       if (!error2){
         if (result!=""){
-          res.render('users', {result:result.rows});
+          res.render('users', {resultUser:resultUser.rows ,result:result.rows});
         }else{
           message = 'Datos no encontrado';
           res.send(`<script>

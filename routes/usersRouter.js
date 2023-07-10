7d4575d720e61596c21412673b001cb935f298a5
khronos1024 <jeanvesu@gmail.com>
2023-07-10 17:30:17 -0500

    se hace validación para creación de usuario cuando no exista en la base

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -20,44 +20,69 @@ router.get('/', (req, res) => {
         return;
       }      
     });
-
+    
     pool.query(user, (error1, resultUser) => {
-      if (!error1) {
-        idUser = resultUser.rows[0].usuario_id;
-        history = `select p.prefijo, p.descripcion_prefijo , hc.consecutivo, hc.descripcion, 
-        cast (hc.fecha_generacion as VARCHAR(16)) 
-        as fecha 
-        from consecutivo.usuario u,consecutivo.prefijo p,consecutivo.historia_consecutivo hc
-        where hc.prefijo_id = p.prefijo_id  and hc.usuario_id = u.usuario_id
-        and u.usuario_id = '${idUser}'
-        order by fecha_generacion desc`;
-      } else {
-        console.log(error1);
-        message = 'Usuario no encontrado';
-        res.send(`<head>
-                    <link rel="stylesheet" href="../stylesheets/styles.css" >
-                    </head>
-                    <div class="confirmacion-container">
-                      <h1 class="confirmacion-title">${message}</h1>
-                      <button class="confirmacion-button" onclick="redirectToLogin(true)">Aceptar</button>
-                    </div>
-                    <script>
-                    function redirectToLogin(confirm) {
-                      if (confirm) {
-                        window.location.href = '/';
-                      } else {
-                        window.location.href = '/';
+      if (resultUser.rows && resultUser.rows.length > 0 && resultUser.rows[0].usuario_id !== "") {
+        if (!error1) {
+          idUser = resultUser.rows[0].usuario_id;
+          /* console.log(idUser); */
+          history = `select p.prefijo, p.descripcion_prefijo , hc.consecutivo, hc.descripcion, 
+          cast (hc.fecha_generacion as VARCHAR(16)) 
+          as fecha 
+          from consecutivo.usuario u,consecutivo.prefijo p,consecutivo.historia_consecutivo hc
+          where hc.prefijo_id = p.prefijo_id  and hc.usuario_id = u.usuario_id
+          and u.usuario_id = '${idUser}'
+          order by fecha_generacion desc`;
+        } else {
+          console.log(idUser);
+          console.log(error1);
+          message = 'Usuario no encontrado';
+          res.send(`<head>
+                      <link rel="stylesheet" href="../stylesheets/styles.css" >
+                      </head>
+                      <div class="confirmacion-container">
+                        <h1 class="confirmacion-title">${message}</h1>
+                        <button class="confirmacion-button" onclick="redirectToLogin(true)">Aceptar</button>
+                      </div>
+                      <script>
+                      function redirectToLogin(confirm) {
+                        if (confirm) {
+                          window.location.href = '/';
+                        } else {
+                          window.location.href = '/';
+                        }
                       }
-                    }
-                    </script>`
-        );
-      }
+                      </script>`
+          );
+        }
 
-        pool.query(history, (error2, result) => {
-          if (!error2) {
-          if (result != "") {
-              res.render('users', { resultUser: resultUser.rows, result: result.rows });
+          pool.query(history, (error2, result) => {
+            if (!error2) {
+            if (result != "") {
+                res.render('users', { resultUser: resultUser.rows, result: result.rows });
+            } else {
+              message = 'Datos no encontrado';
+              res.send(`<head>
+                          <link rel="stylesheet" href="../stylesheets/styles.css" >
+                          </head>
+                          <div class="confirmacion-container">
+                            <h1 class="confirmacion-title">${message}</h1>
+                            <button class="confirmacion-button"       
+                            onclick="redirectToLogin(true)">Aceptar</button>
+                          </div>
+                          <script>
+                          function redirectToLogin(confirm) {
+                            if (confirm) {
+                              window.location.href = '/';
+                            } else {
+                              window.location.href = '/';
+                            }
+                          }
+                          </script>`
+              );
+            }
           } else {
+            console.log(error2);
             message = 'Datos no encontrado';
             res.send(`<head>
                         <link rel="stylesheet" href="../stylesheets/styles.css" >
@@ -77,29 +102,25 @@ router.get('/', (req, res) => {
                         </script>`
             );
           }
-        } else {
-          console.log(error2);
-          message = 'Datos no encontrado';
-          res.send(`<head>
-                      <link rel="stylesheet" href="../stylesheets/styles.css" >
-                      </head>
-                      <div class="confirmacion-container">
-                        <h1 class="confirmacion-title">${message}</h1>
-                        <button class="confirmacion-button" onclick="redirectToLogin(true)">Aceptar</button>
-                      </div>
-                      <script>
-                      function redirectToLogin(confirm) {
-                        if (confirm) {
-                          window.location.href = '/';
-                        } else {
-                          window.location.href = '/';
-                        }
-                      }
-                      </script>`
-          );
+        });
+    }else{
+      const create = `insert into consecutivo.usuario (usuario_ldap)
+      values ('${userldap}')`;
+      pool.query(create, (error, result) => {
+        try{
+        console.log("usuario creado");
+        message = 'Comuniquese con el administrador para habilitar su usuario en la plataforma';
+        res.send(`<script>
+                  alert('"${message}"');
+                  window.location.href = '/';
+              </script>`);
+        }catch(error){
+          console.log(error);
+          res.render(login);
         }
       });
-    }); 
+    }
+    });
   }
 });
 

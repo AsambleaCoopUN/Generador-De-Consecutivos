44eb2b415e4a823225323c8059d8e23a2a266474
khronos1024 <jeanvesu@gmail.com>
2023-07-11 15:44:28 -0500

    se agrega limitador a las conexiónes y al tiempo del servidor

--- src/index.js
+++ src/index.js
@@ -7,5 +7,8 @@ import { PORT } from './config'; // se recibe el puerto para l servidor
 const server = http.createServer(app);
 const httpServer = server.listen(PORT);
 
+server.maxConnections = 20;
+server.timeout = 60000;
+
 const io = new websocketserver(httpServer);
 sockets(io);

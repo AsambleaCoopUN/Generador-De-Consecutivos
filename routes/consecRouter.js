e01e4906a728caf7063035cc203878d472e93432
khronos1024 <jeanvesu@gmail.com>
2023-09-22 17:34:54 -0500

    se realizaron ajustes al concepto de solicitud de consecutivos, y modificación de la consulta en la vista list

--- routes/consecRouter.js
+++ routes/consecRouter.js
@@ -8,9 +8,12 @@ router.get('/', (req, res) => {
       return console.error('Error adquiriendo el cliente', err.stack);
     }
 
-    pool.query(`select hc.usuario_id, hc.consecutivo, hc.descripcion, 
+    pool.query(`select hc.usuario_id, u.usuario_nombre, p.prefijo, p.descripcion_prefijo, hc.consecutivo, hc.descripcion, 
     cast (hc.fecha_generacion as VARCHAR(16)) 
-    as fecha from consecutivo.historia_consecutivo hc`, (error, result) => {
+    as fecha
+    from consecutivo.historia_consecutivo hc, consecutivo.prefijo p, consecutivo.usuario u 
+    where hc.prefijo_id = p.prefijo_id
+    and hc.usuario_id = u.usuario_id`, (error, result) => {
       release(); // Liberar el cliente después de usarlo
 
       try {
